b76bfb0d4bc4a76212b9e3f83d182c9912074743
Abdulsamed25 <abdulsamadamanullah25@gmail.com>
2026-09-10 15:49:58 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,69 @@ def html():
 def health():
     return jsonify({
         "status":"ok",
+        "service":"Nova AI Agent"
+    })
 
+@app.route("/agent",method=["post"])
+def agent():
+  try:
+    data = request.get_json(silent=true) or {}
+    command = data.get("command","").strip()
+    if not command:
+      return jsonify({
+        "success": false,
+        "message":"command is required"
+      }),400
+
+    if not is_email_command(command):
+      return jsonify({
+        "success": False,
+        "message":"please give a gmail commands."
+      }),400
+
+recipient = extract_email(command)
+
+email=generate_email_with_gemini(command)
+
+return jsonify({
+  "success": True,
+  "type":"email"
+  "email_generated": True,
+  "recipient":recipient,
+  "subject":email["body"],
+  "gmail_url":create_gmail_url(
+    email["subject"],
+    email["body"],
+    recipient
+  )
+})
+
+except Exception as e:
+     return jsonify({
+       "success": False,
+       "message": str(e)
+     }),500
+
+return app
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+        
 
 
 
